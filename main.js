e50bc27db39588edbd52a960fdfab6790447fd94
m1cbook <bohdanmylymuk@gmail.com>
2025-11-13 16:45:27 +0200

    com

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // --- Головна сторінка ---
     const video = document.getElementById('myVideo');
     if (video) {
         const skipVideo = localStorage.getItem('skipVideo');
-        if (!skipVideo) {
-            // пробуємо відтворити відео
-            video.play().catch(err => console.log('Авто-відтворення не спрацювало:', err));
+        if (skipVideo) {
+            video.pause(); // якщо треба пропустити відео
         }
-        // скидаємо прапорець після завантаження
         localStorage.removeItem('skipVideo');
     }
+
 
     // --- Інша сторінка ---
     const goHomeLink = document.getElementById('goHomeLink');
